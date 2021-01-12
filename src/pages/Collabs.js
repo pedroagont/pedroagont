2fc46c7af5778062d2ea320c5e7f1b4b98809bde
Pedro A. González <pgonzalez@post.com>
2021-01-12 07:14:42 -0500

    Add array with details and call deck component

--- src/pages/Collabs.js
+++ src/pages/Collabs.js
@@ -1,9 +1,92 @@
-function Collabs() {
+import React from 'react';
+import { Container } from 'reactstrap';
+import Header from '../components/Header';
+import Deck from '../components/Deck';
+
+const Collabs = (props) => {
+  const collabsArray = [
+    {
+      id: 1,
+      img: 'https://picsum.photos/300/200',
+      title: 'CANIETI',
+      category: 'tech, social',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'collabs/canieti',
+    },
+    {
+      id: 2,
+      img: 'https://picsum.photos/300/200',
+      title: 'Acustik',
+      category: 'tech, social',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'collabs/acustik',
+    },
+    {
+      id: 3,
+      img: 'https://picsum.photos/300/200',
+      title: 'Incuvita',
+      category: 'tech, social',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'collabs/incuvita',
+    },
+    {
+      id: 4,
+      img: 'https://picsum.photos/300/200',
+      title: 'Emprendible',
+      category: 'tech, social',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'collabs/emprendible',
+    },
+    {
+      id: 5,
+      img: 'https://picsum.photos/300/200',
+      title: 'Pitch Awards',
+      category: 'tech, social',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'collabs/pitchawards',
+    },
+    {
+      id: 6,
+      img: 'https://picsum.photos/300/200',
+      title: 'Pecha Kucha Nights',
+      category: 'tech, social',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'collabs/pechakucha',
+    },
+    {
+      id: 7,
+      img: 'https://picsum.photos/300/200',
+      title: 'FuckUp Nights',
+      category: 'tech, social',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'collabs/fuckup',
+    },
+    {
+      id: 8,
+      img: 'https://picsum.photos/300/200',
+      title: 'IMJUVE',
+      category: 'tech, social',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'collabs/imjuve',
+    },
+    {
+      id: 9,
+      img: 'https://picsum.photos/300/200',
+      title: 'Alianza del Pacífico',
+      category: 'tech, social',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'collabs/alianzadelpacifico',
+    },
+  ]
+
   return (
     <>
-      <h1>Collabs</h1>
+      <Header title="Collabs" description="This is a modified jumbotron that occupies the entire horizontal space of its parent." />
+      <Container className="mb-5">
+        <Deck array={collabsArray}/>
+      </Container>
     </>
   );
-}
+};
 
 export default Collabs;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,68 @@
-function Projects() {
+import React from 'react';
+import { Container } from 'reactstrap';
+import Header from '../components/Header';
+import Deck from '../components/Deck';
+
+const Projects = (props) => {
+  const projectsArray = [
+    {
+      id: 1,
+      img: 'https://picsum.photos/300/200',
+      title: 'Kaanbal',
+      category: 'tech, social, entrepreneurship',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'projects/kaanbal',
+    },
+    {
+      id: 2,
+      img: 'https://picsum.photos/300/200',
+      title: 'Neonwave',
+      category: 'digital marketing, tech',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'projects/neonwave',
+    },
+    {
+      id: 3,
+      img: 'https://picsum.photos/300/200',
+      title: 'DEV.F',
+      category: 'digital marketing, tech',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'projects/devf',
+    },
+    {
+      id: 4,
+      img: 'https://picsum.photos/300/200',
+      title: 'Tech Garage',
+      category: '#digitalmarketing #tech',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'projects/techgarage',
+    },
+    {
+      id: 5,
+      img: 'https://picsum.photos/300/200',
+      title: 'Cerebro Digital',
+      category: 'digital marketing, tech',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'projects/cerebrodigital',
+    },
+    {
+      id: 6,
+      img: 'https://picsum.photos/300/200',
+      title: 'Internet Society',
+      category: 'digital marketing, tech',
+      description: 'Cool stuff with the Internet bois',
+      slug: 'projects/internetsociety',
+    },
+  ]
+
   return (
     <>
-      <h1>Projects</h1>
+      <Header title="Projects" description="This is a modified jumbotron that occupies the entire horizontal space of its parent." />
+      <Container className="mb-5">
+        <Deck array={projectsArray}/>
+      </Container>
     </>
   );
-}
+};
 
 export default Projects;
