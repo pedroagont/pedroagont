e76ca1f8857ad8ba9e5e0012352653c2a447ae9a
Pedro A. González <pgonzalez@post.com>
2021-01-12 07:14:04 -0500

    Add info details

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,19 @@
-function Contact() {
+import React from 'react';
+import { Container } from 'reactstrap';
+import Header from '../components/Header';
+
+const Contact = (props) => {
   return (
     <>
-      <h1>Contact</h1>
+      <Header title="Contact" description="This is a modified jumbotron that occupies the entire horizontal space of its parent." />
+      <Container className="text-center font-weight-bold">
+        <p>Pedro A. González</p>
+        <p>+52 (998) 845 8554</p>
+        <p>pgonzalez@post.com</p>
+        <p>@pedroagont</p>
+      </Container>
     </>
   );
-}
+};
 
 export default Contact;
