4d6c54014974646ef9b8222d656a212b89ccacff
Pedro A. González <pgonzalez@post.com>
2021-01-12 07:10:26 -0500

    Add details to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,16 @@ import { FaFacebook, FaGithub, FaInstagram, FaTwitter, FaTwitch } from "react-ic
 
 const Example = (props) => {
   return (
-    <footer className="footer mt-auto py-3 bg-dark">
+    <footer className="footer mt-auto py-3">
+      <hr className="w-75 mt-5" />
       <Container className="text-center">
-        <a href="#" className="text-muted px-2"><FaInstagram /></a>
-        <a href="#" className="text-muted px-2"><FaGithub /></a>
-        <a href="#" className="text-muted px-2"><FaFacebook /></a>
-        <a href="#" className="text-muted px-2"><FaTwitter /></a>
-        <a href="#" className="text-muted px-2"><FaTwitch /></a>
-        <br />
-        <br />
-        <span className="text-muted">Pedro A. González © { new Date().getFullYear() }. All Rights Reserved</span>
+        <a href="https://instagram.com/pedroagont" className="text-dark px-3"><FaInstagram /></a>
+        <a href="https://github.com/pedroagont" className="text-dark px-3"><FaGithub /></a>
+        <a href="https://facebook.com/pedroagont" className="text-dark px-3"><FaFacebook /></a>
+        <a href="https://twitter.com/pedroagont" className="text-dark px-3"><FaTwitter /></a>
+        <a href="https://twitch.com/pedroagont" className="text-dark px-3"><FaTwitch /></a>
+        <div style={{ height: '1.5em' }} />
+        <span className="text-dark">Pedro A. González © { new Date().getFullYear() }. All Rights Reserved</span>
       </Container>
     </footer>
   );
