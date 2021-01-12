1ca525872ebc07ff8f2c56d908aeacab47820e5d
Pedro A. González <pgonzalez@post.com>
2021-01-12 07:15:20 -0500

    Add cardcolumns components from reactstrap

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,9 +1,36 @@
-function Gallery() {
+import React from 'react';
+import { Container } from 'reactstrap';
+import Header from '../components/Header';
+import { Card, CardImg, CardColumns } from 'reactstrap';
+
+const Gallery = (props) => {
   return (
     <>
-      <h1>Gallery</h1>
+      <Header title="Gallery" description="This is a modified jumbotron that occupies the entire horizontal space of its parent." />
+      <Container>
+        <CardColumns>
+          <Card>
+            <CardImg top width="100%" src="https://picsum.photos/500/200" alt="Card image cap" />
+          </Card>
+          <Card>
+            <CardImg top width="100%" src="https://picsum.photos/400/300" alt="Card image cap" />
+          </Card>
+          <Card>
+            <CardImg top width="100%" src="https://picsum.photos/600/300" alt="Card image cap" />
+          </Card>
+          <Card>
+            <CardImg top width="100%" src="https://picsum.photos/400/800" alt="Card image cap" />
+          </Card>
+          <Card>
+            <CardImg top width="100%" src="https://picsum.photos/300/200" alt="Card image cap" />
+          </Card>
+          <Card>
+            <CardImg top width="100%" src="https://picsum.photos/1020/800" alt="Card image cap" />
+          </Card>
+        </CardColumns>
+      </Container>
     </>
   );
-}
+};
 
 export default Gallery;
