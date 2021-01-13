6d3f7e66143f3ee8eb547e63b3c9afd8e621200e
Pedro A. González <pgonzalez@post.com>
2021-01-13 05:11:54 -0500

    Refactor gallery with images component

---
+++ src/components/Images.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card, CardImg, CardColumns } from 'reactstrap';
+
+const Images = (props) => {
+  return (
+    <CardColumns>
+      {
+        props.array.map(el => {
+          return <Card>
+                    <CardImg width="100%" src={ el.src } alt={ el.alt } />
+                 </Card>
+        })
+      }
+    </CardColumns>
+  );
+};
+
+export default Images;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,33 +1,77 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import Header from '../components/Header';
-import { Card, CardImg, CardColumns } from 'reactstrap';
+import Images from '../components/Images';
 
 const Gallery = (props) => {
+  const galleryArray = [
+    {
+      id: 1,
+      src: 'https://picsum.photos/500/200',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 2,
+      src: 'https://picsum.photos/400/300',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 3,
+      src: 'https://picsum.photos/600/300',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 4,
+      src: 'https://picsum.photos/400/800',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 5,
+      src: 'https://picsum.photos/300/200',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 6,
+      src: 'https://picsum.photos/1020/800',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 7,
+      src: 'https://picsum.photos/500/200',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 8,
+      src: 'https://picsum.photos/400/300',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 9,
+      src: 'https://picsum.photos/600/300',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 10,
+      src: 'https://picsum.photos/400/800',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 11,
+      src: 'https://picsum.photos/300/200',
+      alt: 'Cool stuff',
+    },
+    {
+      id: 12,
+      src: 'https://picsum.photos/1020/800',
+      alt: 'Cool stuff',
+    },
+  ]
+
   return (
     <>
       <Header title="Gallery" description="This is a modified jumbotron that occupies the entire horizontal space of its parent." />
       <Container>
-        <CardColumns>
-          <Card>
-            <CardImg top width="100%" src="https://picsum.photos/500/200" alt="Card image cap" />
-          </Card>
-          <Card>
-            <CardImg top width="100%" src="https://picsum.photos/400/300" alt="Card image cap" />
-          </Card>
-          <Card>
-            <CardImg top width="100%" src="https://picsum.photos/600/300" alt="Card image cap" />
-          </Card>
-          <Card>
-            <CardImg top width="100%" src="https://picsum.photos/400/800" alt="Card image cap" />
-          </Card>
-          <Card>
-            <CardImg top width="100%" src="https://picsum.photos/300/200" alt="Card image cap" />
-          </Card>
-          <Card>
-            <CardImg top width="100%" src="https://picsum.photos/1020/800" alt="Card image cap" />
-          </Card>
-        </CardColumns>
+        <Images array={ galleryArray } />
       </Container>
     </>
   );
