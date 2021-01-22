e9a247ef7133cc836b3e24372543828a7974878a
Pedro A. González <pgonzalez@post.com>
2021-01-21 22:26:27 -0500

    Add whatsapp, increase icons size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-import { FaFacebook, FaGithub, FaInstagram, FaTwitter, FaTwitch } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter, FaTwitch, FaWhatsapp } from "react-icons/fa";
 
 const Example = (props) => {
   return (
     <footer className="footer mt-auto py-3">
       <hr className="w-75 mt-5" />
       <Container className="text-center">
-        <a href="https://instagram.com/pedroagont" className="text-dark px-3"><FaInstagram /></a>
-        <a href="https://github.com/pedroagont" className="text-dark px-3"><FaGithub /></a>
-        <a href="https://facebook.com/pedroagont" className="text-dark px-3"><FaFacebook /></a>
-        <a href="https://twitter.com/pedroagont" className="text-dark px-3"><FaTwitter /></a>
-        <a href="https://twitch.com/pedroagont" className="text-dark px-3"><FaTwitch /></a>
+        <a href="https://instagram.com/pedroagont" className="text-dark px-3"><FaInstagram style={{ fontSize: '1.5em' }}/></a>
+        <a href="https://github.com/pedroagont" className="text-dark px-3"><FaGithub style={{ fontSize: '1.5em' }}/></a>
+        <a href="https://facebook.com/pedroagont" className="text-dark px-3"><FaFacebook style={{ fontSize: '1.5em' }}/></a>
+        <a href=" https://wa.me/529988458554" className="text-dark px-3"><FaWhatsapp style={{ fontSize: '1.5em' }}/></a>
+        <a href="https://twitter.com/pedroagont" className="text-dark px-3"><FaTwitter style={{ fontSize: '1.5em' }}/></a>
+        <a href="https://twitch.com/pedroagont" className="text-dark px-3"><FaTwitch style={{ fontSize: '1.5em' }}/></a>
         <div style={{ height: '1.5em' }} />
         <span className="text-dark">Pedro A. González © { new Date().getFullYear() }. All Rights Reserved</span>
       </Container>
