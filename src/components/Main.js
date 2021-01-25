bc1576cd4d1ccae9b516b2ce2427cf6045cbd9d4
Pedro A. González <pgonzalez@post.com>
2021-01-25 17:53:40 -0500

    Update texts

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,24 +8,24 @@ const Main = (props) => {
       <Jumbotron className="text-center bg-transparent" style={{ marginBottom: '0' }}>
         <Container>
           <h1 className="display-3">Bio</h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent. This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          <p className="lead">Pedro A. González is a Mexican educator and technology evangelist known for his contributions to Internet and software development, bridging the digital divide and applied social innovation.</p>
         </Container>
         <Container className="mt-5">
           <Row>
             <Col>
               <h3 className="display-2 text-muted">12+</h3>
               <p className="lead">Awards & Fellowships</p>
-              <p className="small">This is a modified jumbotron that occupies the entire horizontal space of its parent. This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+              <p className="small">Winner in different challenges, grants, and prizes being recognized as an innovator and youth talent of the academic, civil society, private industry, and government community at a local, regional, national and international level.</p>
             </Col>
             <Col>
-              <h3 className="display-2 text-muted">200+</h3>
+              <h3 className="display-2 text-muted">80+</h3>
               <p className="lead">Mentorships</p>
-              <p className="small">This is a modified jumbotron that occupies the entire horizontal space of its parent. This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+              <p className="small">From individuals and startups, to private companies, involving topics related to Internet, science, technology, digital culture, social innovation, robotics, IoT, event production, project management, and content creation.</p>
             </Col>
             <Col>
-              <h3 className="display-2 text-muted">200+</h3>
+              <h3 className="display-2 text-muted">250+</h3>
               <p className="lead">Events participation</p>
-              <p className="small">This is a modified jumbotron that occupies the entire horizontal space of its parent. This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+              <p className="small">As guest speaker and organizer of activities related to science, education, technology, research, innovation and entrepreneurship in Los Angeles, Boston, New York, Toronto, Argentina and all over México, including invitations to Harvard, MIT, and United Nations.</p>
             </Col>
           </Row>
           <div style={{ height: '3em' }} />
