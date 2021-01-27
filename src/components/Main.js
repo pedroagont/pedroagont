f320519bdfbc6da6f52cab0a028a6b2a63593423
Pedro A. González <pgonzalez@post.com>
2021-01-27 05:05:44 -0500

    Remove parent div to avoid nesting tags

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,33 +3,31 @@ import { Jumbotron, Container, Row, Col } from 'reactstrap';
 
 const Main = (props) => {
   return (
-    <div>
-      <Jumbotron className="text-center bg-transparent" style={{ marginBottom: '0' }}>
-        <Container>
-          <h1 className="display-3">Bio</h1>
-          <p className="lead">Pedro A. González is a Mexican educator and technology evangelist known for his contributions to Internet and software development, digital inclusion and applied social innovation.</p>
-        </Container>
-        <Container className="mt-5">
-          <Row>
-            <Col>
-              <h3 className="display-2 text-muted">12+</h3>
-              <p className="lead">Awards & Fellowships</p>
-              <p className="small">Winner in different challenges, grants, and prizes being recognized as an innovator and youth talent of the academic, civil society, private industry, and government community at a local, regional, national and international level.</p>
-            </Col>
-            <Col>
-              <h3 className="display-2 text-muted">80+</h3>
-              <p className="lead">Mentorships</p>
-              <p className="small">From individuals and startups, to private companies, involving topics related to Internet, science, technology, digital culture, social innovation, robotics, IoT, event production, project management, and content creation.</p>
-            </Col>
-            <Col>
-              <h3 className="display-2 text-muted">250+</h3>
-              <p className="lead">Events participation</p>
-              <p className="small">As guest speaker and organizer of activities related to science, education, technology, research, innovation and entrepreneurship in Los Angeles, Boston, New York, Toronto, Argentina and all over México, including invitations to Harvard, MIT, and United Nations.</p>
-            </Col>
-          </Row>
-        </Container>
-      </Jumbotron>
-    </div>
+    <Jumbotron className="text-center bg-transparent" style={{ marginBottom: '0' }}>
+      <Container>
+        <h1 className="display-3">Bio</h1>
+        <p className="lead">Pedro A. González is a Mexican educator and technology evangelist known for his contributions to Internet and software development, digital inclusion and applied social innovation.</p>
+      </Container>
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <h3 className="display-2 text-muted">12+</h3>
+            <p className="lead">Awards & Fellowships</p>
+            <p className="small">Winner in different challenges, grants, and prizes being recognized as an innovator and youth talent of the academic, civil society, private industry, and government community at a local, regional, national and international level.</p>
+          </Col>
+          <Col>
+            <h3 className="display-2 text-muted">80+</h3>
+            <p className="lead">Mentorships</p>
+            <p className="small">From individuals and startups, to private companies, involving topics related to Internet, science, technology, digital culture, social innovation, robotics, IoT, event production, project management, and content creation.</p>
+          </Col>
+          <Col>
+            <h3 className="display-2 text-muted">250+</h3>
+            <p className="lead">Events participation</p>
+            <p className="small">As guest speaker and organizer of activities related to science, education, technology, research, innovation and entrepreneurship in Los Angeles, Boston, New York, Toronto, Argentina and all over México, including invitations to Harvard, MIT, and United Nations.</p>
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
   );
 };
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -15,43 +15,41 @@ const Navigation = (props) => {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div>
-      <Navbar scrolling="true" light expand="md">
-        <NavbarBrand className="" href="/">pedroagont</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <Link to="/" className="text-dark px-3" onClick={toggle} >Home</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/about" className="text-dark px-3" onClick={toggle} >About</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/projects" className="text-dark px-3" onClick={toggle} >Projects</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/collabs" className="text-dark px-3" onClick={toggle} >Collabs</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/alumni" className="text-dark px-3" onClick={toggle} >Alumni</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/gallery" className="text-dark px-3" onClick={toggle} >Gallery</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/press" className="text-dark px-3" onClick={toggle} >Press</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/cv" className="text-dark px-3" onClick={toggle} >CV</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/contact" className="text-dark px-3" onClick={toggle} >Contact</Link>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
+    <Navbar scrolling="true" light expand="md">
+      <NavbarBrand className="" href="/">pedroagont</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <Link to="/" className="text-dark px-3" onClick={toggle} >Home</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/about" className="text-dark px-3" onClick={toggle} >About</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/projects" className="text-dark px-3" onClick={toggle} >Projects</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/collabs" className="text-dark px-3" onClick={toggle} >Collabs</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/alumni" className="text-dark px-3" onClick={toggle} >Alumni</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/gallery" className="text-dark px-3" onClick={toggle} >Gallery</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/press" className="text-dark px-3" onClick={toggle} >Press</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/cv" className="text-dark px-3" onClick={toggle} >CV</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/contact" className="text-dark px-3" onClick={toggle} >Contact</Link>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
   );
 }
 
