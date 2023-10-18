62832105f5cf0ccd344f6607bf6532d6ed223c74
Pedro A. González <pgonzalez@post.com>
2023-10-17 19:44:40 -0600

    Add react router dom with simple components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,71 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 import '../styles/App.css';
+
+function Navigation() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">pedroagont</Link>
+      </div>
+      <ul className="navbar-menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function Header({ title, description }) {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </header>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <Header title="Home" description="Welcome" />
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <Header title="About" description="A little bit about me" />
+    </>
+  );
+}
+
+function Projects() {
+  return (
+    <>
+      <Header title="Projects" description="Some projects I've worked around" />
+    </>
+  );
+}
 
 function App() {
   return (
-    <h1>Hello!</h1>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
