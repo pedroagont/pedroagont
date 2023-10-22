edb4976fd39686fb27898a45b380107ec348d64a
Pedro A. González <pgonzalez@post.com>
2023-10-22 15:58:45 -0600

    Refactor nav into dynamic map, conditional styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,33 +1,56 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
 
 function Navigation() {
+  const location = useLocation();
+
+  const menuItemSelected = (item) =>
+    classNames({
+      'menu-item-selected': item.pathname === location.pathname,
+    });
+
+  const menuListData = [
+    {
+      name: 'Home',
+      pathname: '/',
+    },
+    {
+      name: 'About',
+      pathname: '/about',
+    },
+    {
+      name: 'Experience',
+      pathname: '/experience',
+    },
+    {
+      name: 'Education',
+      pathname: '/education',
+    },
+    {
+      name: 'Projects',
+      pathname: '/projects',
+    },
+    {
+      name: 'Dev',
+      pathname: '/dev',
+    },
+    {
+      name: 'Contact',
+      pathname: '/contact',
+    },
+  ];
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <Link to="/">@pedroagont</Link>
       </div>
       <ul className="navbar-menu">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/experience">Experience</Link>
-        </li>
-        <li>
-          <Link to="/education">Education</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/dev">Dev</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
+        {menuListData.map((item) => (
+          <li key={item.name} className={menuItemSelected(item)}>
+            <Link to={item.pathname}>{item.name}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
