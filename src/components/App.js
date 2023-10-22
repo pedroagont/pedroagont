5b5b2daa91b9f23ce3765d94a182485091fd9e08
Pedro A. González <pgonzalez@post.com>
2023-10-22 13:17:20 -0600

    Adjust text details and styling

--- src/components/App.js
+++ src/components/App.js
@@ -50,10 +50,12 @@ function Home() {
       <Header title="Home" description="Welcome" />
       <main className="container">
         <p>
-          Pedro A. González is a tech professional with a track record of making
-          technology accessible and impactful, supported by a solid educational
-          background and a commitment to knowledge sharing and innovation.
+          Pedro A. González is a mexican engineer and multi-faceted individual
+          with a track record of making technology accessible and impactful. His
+          visionary leadership has brought positive change through innovation,
+          education, and an unwavering passion for technology.
         </p>
+        <Link to="/about">See more</Link>
       </main>
     </>
   );
@@ -69,19 +71,22 @@ function About() {
           innovation with the mission of making technology accesible and
           impactful.
           <br /> <br />
-          He is the co-founder of Kaanbal.org, a non profit organization focused
-          on connecting remote communities with Internet access and tech-driven
-          education.
+          He is the co-founder of{' '}
+          <a href="https://kaanbal.org" target="_blank" rel="noreferrer">
+            Kaanbal.org
+          </a>
+          , a non profit organization focused on connecting remote communities
+          with Internet access and tech-driven education.
           <br /> <br />
-          His journey as an educator and tech entrepreneur have contributed a
-          significantly into bridging the knowledge and digital divide,
-          positively impacting the lives of thousands.
+          His journey as an innovator and tech entrepreneur have contributed
+          significantly to boost the knowledge-based ecosystem and bridge the
+          digital divide, positively impacting the lives of thousands.
           <br /> <br />
-          With a background in Mechatronics Engineering, a Master in Software
-          Development and a specialization in Future Tech, Mr. González is a
-          high end professional in the field of technology.
+          With a background in Mechatronics Engineering, a Master in Coding and
+          a specialization in Entrepreneurship and Future Tech, Mr. González is
+          a high end professional in the field of technology.
           <br /> <br />
-          His passion on the topic has led Mr. González to participate as an
+          His passion on the topic has led him to participate as an
           international guest speaker, lecturer and presenter in dozens of
           events around Canada, United States, Mexico, Argentina, Germany and
           Japan, which features invitations from prestigious institutions such
@@ -150,7 +155,7 @@ function Experience() {
       id: 7,
       company:
         'Mexican Chamber of Electronics, Telecommunications and Information Technologies (CANIETI)',
-      position: 'Events Coordinator',
+      position: 'Events Coordinator Internship',
       location: 'Cancun, MX',
       fromTo: '2015 - 2016',
       url: 'http://www.canieti.org/Inicio/English/englishcan/about.aspx',
@@ -159,10 +164,7 @@ function Experience() {
 
   return (
     <>
-      <Header
-        title="Experience"
-        description="Companies that have trusted my work"
-      />
+      <Header title="Experience" description="Professional background" />
       <main className="container">
         <ul className="experience-list">
           {data.map((e) => (
@@ -173,10 +175,12 @@ function Experience() {
                   <span>{e.location}</span>
                   <span> | </span>
                   <span>{e.fromTo}</span>
-                </div>{' '}
+                </div>
               </h3>
               <p>{e.position}</p>
-              <a href={e.url}>More information</a>
+              <a href={e.url} target="_blank" rel="noreferrer">
+                See more
+              </a>
             </li>
           ))}
         </ul>
@@ -194,7 +198,7 @@ function Education() {
         'Emergent Future Technologies Certificate (Neurotech, Biomedics, IoT, Data Science, AI/ML, AR/VR)',
       location: 'Osaka, JP',
       fromTo: '2023',
-      url: 'http://example.com',
+      url: 'https://www.mirai-innovation-lab.com',
     },
     {
       id: 2,
@@ -203,7 +207,7 @@ function Education() {
         'Master in Coding (Fullstack Development Javascript, Git, React, Express, MongoDB, Postgresql)',
       location: 'Mexico City, MX',
       fromTo: '2018 - 2019',
-      url: 'http://example.com',
+      url: 'https://new.devf.la/en',
     },
     {
       id: 3,
@@ -212,7 +216,7 @@ function Education() {
         'High Impact Entrepreneurship Certificate (Startup ecosystem, Agile methodology, Intellectual Property)',
       location: 'Merida, MX',
       fromTo: '2015 - 2016',
-      url: 'http://example.com',
+      url: 'https://merida.anahuac.mx/tecnia',
     },
     {
       id: 4,
@@ -221,13 +225,13 @@ function Education() {
         'B.S. in Mechatronics Engineering (Mechanics, Electronics, Software, Control Engineering)',
       location: 'Cancun, MX',
       fromTo: '2011 - 2016',
-      url: 'http://example.com',
+      url: 'https://www.cancun.tecnm.mx',
     },
   ];
 
   return (
     <>
-      <Header title="Education" description="Educational timeline" />
+      <Header title="Education" description="Academic timeline" />
       <main className="container">
         <ul className="education-list">
           {data.map((e) => (
@@ -238,10 +242,12 @@ function Education() {
                   <span>{e.location}</span>
                   <span> | </span>
                   <span>{e.fromTo}</span>
-                </div>{' '}
+                </div>
               </h3>
               <p>{e.achievement}</p>
-              <a href={e.url}>More information</a>
+              <a href={e.url} target="_blank" rel="noreferrer">
+                See more
+              </a>
             </li>
           ))}
         </ul>
@@ -257,7 +263,9 @@ function ProjectsListItem({ name, description, url, image }) {
       <div>
         <h3>{name}</h3>
         <p>{description}</p>
-        <a href={url}>{url}</a>
+        <a href={url} target="_blank" rel="noreferrer">
+          See more
+        </a>
       </div>
     </li>
   );
@@ -267,58 +275,66 @@ function Projects() {
   const data = [
     {
       id: 1,
-      name: 'Kaanbal',
-      description: 'An AI platform',
-      url: 'http://example.com',
+      name: 'Kaanbal.org',
+      description:
+        'Nonprofit organization with the mission to connect remote communities with Internet access and tech-driven education.',
+      url: 'https://kaanbal.org',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 2,
       name: 'Global Shapers Cancun Hub',
-      description: 'An AI platform',
-      url: 'http://example.com',
+      description:
+        'Youth empowerment initiative with the World Economic Foum to promote citizenship and social impact.',
+      url: 'https://globalshaperscancun.org',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 3,
-      name: 'Neonwave',
-      description: 'An AI platform',
-      url: 'http://example.com',
+      name: 'Neonwave.mx',
+      description:
+        'Digital marketing agency delivering web development, branding and graphic design.',
+      url: 'https://neonwave.mx',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 4,
       name: 'Turepa.mx',
-      description: 'An AI platform',
-      url: 'http://example.com',
+      description:
+        'Delivery people platform for your business at the tip of your fingers.',
+      url: 'https://www.turepa.mx',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 5,
       name: 'Cerebro Digital',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      description:
+        'Educational content to promote science and technology to a 4 million followers community',
+      url: 'https://facebook.com/tucerebrodigital',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 6,
       name: 'Singulary.ai',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      description:
+        'A singular AI platform to prompt questions, images, music, video and code.',
+      url: 'https://singulary.ai',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 7,
       name: 'TicketFlyers.com',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      description:
+        'Global search engine for purchasing flights, hotels, tours and car rentals.',
+      url: 'https://ticketflyers.com',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 8,
       name: 'Bouncing Bob',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      description:
+        'Simple tap game to stack endless hours of fun, available on Android and iOS',
+      url: 'https://play.google.com/store/apps/details?id=com.game.bouncingbob.play',
       image: 'https://picsum.photos/300/200',
     },
   ];
@@ -337,14 +353,21 @@ function Projects() {
   );
 }
 
-function DevListItem({ name, description, url, image }) {
+function DevListItem({ name, description, url, repository, image }) {
   return (
     <li className="dev-list-item">
       <img src={image} alt={image} />
       <div>
         <h3>{name}</h3>
         <p>{description}</p>
-        <a href={url}>{url}</a>
+        <span className="dev-list-item-links">
+          <a href={url} target="_blank" rel="noreferrer">
+            Link
+          </a>
+          <a href={repository} target="_blank" rel="noreferrer">
+            Repository
+          </a>
+        </span>
       </div>
     </li>
   );
@@ -355,99 +378,113 @@ function Dev() {
     {
       id: 1,
       name: 'Herbiview',
-      description: 'An AI platform',
-      url: 'http://example.com',
+      description:
+        'Identify plants pictures uploaded by users using AI and learn from it.',
+      url: 'http://herbiview.onrender.com',
+      repository: 'https://github.com/pedroagont/herbiview',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 2,
-      name: 'Shopify Tech Assesment',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      name: 'AI Tech Assessment',
+      description:
+        'Frontend role task using OpenAI for a job position in Shopify.',
+      url: 'https://shopify-technical-assignment.vercel.app',
+      repository: 'https://github.com/amyleblanc/shopify-technical-assignment',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 3,
       name: 'Pokemon Matching Game',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      description:
+        'Awesome and simple matching card game using Pokemon characters.',
+      url: 'https://norbjz.github.io/memorama-pokemon',
+      repository: 'https://github.com/Norbjz/memorama-pokemon',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 4,
       name: 'Women Violencemeter',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      description:
+        'Survey to evaluate violence during International Women Day.',
+      url: 'https://mo0sh.github.io/violentometro',
+      repository: 'https://github.com/mo0sh/violentometro',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 5,
       name: 'Studeecloud',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      description:
+        'Collaborative dashboard that integrates a range of tools like pomodoro timer and video calls to stay on track.',
+      url: 'https://studeecloud.onrender.com',
+      repository: 'https://github.com/pedroagont/studeecloud',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 6,
       name: 'The Shoebox',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      description:
+        'E-commerce website to browse through a collection of shoes where you can filter the search based on style, color and price.',
+      url: 'https://shoe-box.netlify.app',
+      repository: 'https://github.com/FarzanehSa/shoeBox-client',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 7,
-      name: 'SmartArt',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      name: 'Raspberry Pi + Python + TouchOSC',
+      description: 'Remote controlled robots to livestream video.',
+      url: 'https://pedroagont.tumblr.com/post/137541587893#137541587893',
+      repository: 'https://github.com/pedroagont/py-carosc',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 8,
-      name: 'Raspberry Pi + Robotics',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      name: 'React + Giphy SDK',
+      description: 'Gif searcher using the Giphy SDK and API.',
+      url: 'https://react-cool-gifs.netlify.app',
+      repository: 'https://github.com/pedroagont/react-cool-gifs',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 9,
-      name: 'React + Giphy SDK',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      name: 'React + ThreeJS',
+      description:
+        'Experiment using 3d modeling and browser events with ThreeJS',
+      url: 'https://react-three-js.netlify.app',
+      repository: 'https://github.com/pedroagont/react-three-js',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 10,
-      name: 'React + ThreeJS',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      name: 'React + Bootstrap',
+      description: 'Demo on using React with Bootstrap.',
+      url: 'https://react-bootstrap-g7.netlify.app',
+      repository:
+        'https://github.com/pedroagont/master-code-g7/blob/master/05_Frontend/07_react-bootstrap',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 11,
-      name: 'React + Bootstrap',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      name: 'VanillaJS Avatar Maker',
+      description: 'Create a fun avatar image and download it.',
+      url: 'https://pedroagont.github.io/mochi-maker/',
+      repository: 'https://github.com/pedroagont/mochi-maker',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 12,
-      name: 'VanillaJS Avatar Maker',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      name: 'VanillaJS NASA Image Searcher',
+      description: 'Search images on NASA archive using the NASA API.',
+      url: 'https://pedroagont.github.io/steam-fair-conference-8apr',
+      repository: 'https://github.com/pedroagont/steam-fair-conference-8apr',
       image: 'https://picsum.photos/300/200',
     },
     {
       id: 13,
-      name: 'VanillaJS NASA Image Searcher',
-      description: 'A simple tap game',
-      url: 'http://example.com',
-      image: 'https://picsum.photos/300/200',
-    },
-    {
-      id: 14,
-      name: 'VanillaJS Color Flicker',
-      description: 'A simple tap game',
-      url: 'http://example.com',
+      name: 'VanillaJS CSS Color Flicker',
+      description: 'Select a range of CSS colors using this fun tool.',
+      url: 'https://pedroagont.github.io/The-Flicker',
+      repository: 'https://github.com/pedroagont/The-Flicker',
       image: 'https://picsum.photos/300/200',
     },
   ];
@@ -472,10 +509,56 @@ function Dev() {
 function Contact() {
   return (
     <>
-      <Header
-        title="Contact"
-        description="Feel free to reach if I can help in any way"
-      />
+      <Header title="Contact" description="Let's collaborate" />
+      <main className="container">
+        <div className="contact-links">
+          <a href="/" target="_blank" rel="noreferrer">
+            pgonzalez@post.com
+          </a>
+          <a
+            href="https://github.com/pedroagont"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://instagram.com/pedroagont/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://facebook.com/pedroagont"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Facebook
+          </a>
+          <a
+            href="https://reddit.com/user/pedroagont"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Reddit
+          </a>
+          <a
+            href="https://twitter.com/pedroagont"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://linkedin.com/in/pedroagont"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </main>
     </>
   );
 }
