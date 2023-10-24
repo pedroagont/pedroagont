ed2dfde110d23fe4b919b1b414927d164b3464ab
Pedro A. González <pgonzalez@post.com>
2023-10-23 20:03:17 -0600

    Add responsive to navigation and few other styling details

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 
 function Navigation({ data }) {
+  const [collapse, setCollapse] = useState(window.innerWidth < 570);
   const location = useLocation();
 
   const menuItemSelected = (item) =>
@@ -9,18 +11,36 @@ function Navigation({ data }) {
       'menu-item-selected': item.pathname === location.pathname,
     });
 
+  const handleClickToggleMenu = () => {
+    setCollapse(!collapse);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', (e) => {
+      setCollapse(e.target.innerWidth < 570);
+    });
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <Link to="/">@pedroagont</Link>
       </div>
-      <ul className="navbar-menu">
-        {data.map((item) => (
-          <li key={item.name} className={menuItemSelected(item)}>
-            <Link to={item.pathname}>{item.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="navbar-button" onClick={handleClickToggleMenu}>
+        <i className="fa-solid fa-bars"></i>
+      </div>
+      <div
+        className="navbar-collapse"
+        style={{ display: collapse ? 'none' : 'block' }}
+      >
+        <ul className="navbar-collapse-menu">
+          {data.map((item) => (
+            <li key={item.name} className={menuItemSelected(item)}>
+              <Link to={item.pathname}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 }
